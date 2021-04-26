1faf7873a205bd4cfee8ce96ca7b0e423b64f914
davidgabriela <david.gabriela.99@gmail.com>
2021-04-26 20:53:38 +0300

    Cleanup

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "BoxShady")
+            Platform.button = true;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "BoxShady")
+            Platform.button = false;
+    }
+}
