b21711259793de3fa068d20caef2d0f911e35f19
unknown <raresgabrielalexe@yahoo.ro>
2021-05-08 19:53:09 +0300

    LightBulb

---
+++ Assets/ControlBulb.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlBulb : MonoBehaviour
+{
+    public bool isOpen = false;
+    public Animator animator;
+
+    public void OpenChest()
+    {
+        if (!isOpen)
+        {
+            isOpen = true;
+            animator.SetBool("isOpen", isOpen);
+        }
+    }
+
+    public void CloseChest()
+    {
+        if (isOpen)
+        {
+            isOpen = false;
+            animator.SetBool("isOpen", isOpen);
+        }
+    }
+}
---
+++ Assets/TurnOn.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TurnOn : MonoBehaviour
+{
+    public bool isInRangeSparky;
+    public KeyCode interactKeySparky;
+    public UnityEvent interactActionSparky;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (isInRangeSparky)
+        {
+            if (Input.GetKeyDown(interactKeySparky))
+            {
+                interactActionSparky.Invoke();
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.gameObject.CompareTag("Sparky"))
+        {
+            isInRangeSparky = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+
+        if (collision.gameObject.CompareTag("Sparky"))
+        {
+            isInRangeSparky = false;
+        }
+    }
+}
