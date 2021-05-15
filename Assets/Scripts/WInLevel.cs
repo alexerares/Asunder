8c38e70a3100b5fab28350d9a8fc8f90282cc8f5
unknown <raresgabrielalexe@yahoo.ro>
2021-05-15 16:27:11 +0300

    UsebleStars

--- Assets/Scripts/WInLevel.cs
+++ Assets/Scripts/WInLevel.cs
@@ -5,6 +5,8 @@ using UnityEngine.SceneManagement;
 public class WInLevel : MonoBehaviour
 {
     // Start is called before the first frame update
+    public bool unlock1 = false;
+    public bool unlock2 = false;
     void Start()
     {
         
@@ -13,7 +15,7 @@ public class WInLevel : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (transform.GetChild(0).GetComponent<Elevator_exit>().shady && transform.GetChild(1).GetComponent<pipe_exit>().sparky)
+        if (transform.GetChild(0).GetComponent<Elevator_exit>().shady && transform.GetChild(1).GetComponent<pipe_exit>().sparky && unlock1 && unlock2)
         {
             transform.GetChild(0).GetComponent<Elevator_exit>().animator.SetBool("win", true);
             transform.GetChild(1).transform.GetChild(2).GetComponent<Jesus>().animator.SetBool("Jesus", true);
