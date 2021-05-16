86936d832d8e105946690dc65e2f74b622c2e450
unknown <raresgabrielalexe@yahoo.ro>
2021-05-16 22:03:57 +0300

    RestartButton

---
+++ Assets/RestartButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
