e436559236356d10c721946d40620a7b69ea042c
unknown <raresgabrielalexe@yahoo.ro>
2021-05-24 13:18:40 +0300

    Active/Deactive_Pipe

---
+++ Assets/Scripts/GreenWheelActivate.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GreenWheelActivate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool isInRange;
+    public bool on;
+    public KeyCode interactKeyShady;
+    public GameObject pipe;
+    public Animator animator;
+    void Start()
+    {
+        if (on)
+        {
+            animator.SetBool("Activate", on);
+            pipe.GetComponent<GasPipeGreen>().animator.SetBool("Active", on);
+            pipe.transform.GetChild(0).GetComponent<BoxCollider2D>().enabled = on;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isInRange)
+        {
+            if (Input.GetKeyDown(interactKeyShady))
+            {
+                if (!on)
+                {
+                    on = true;
+                    animator.SetBool("Activate", on);
+                    pipe.GetComponent<GasPipeGreen>().animator.SetBool("Active", on);
+                    pipe.transform.GetChild(0).GetComponent<BoxCollider2D>().enabled = on;
+                }
+                else
+                {
+                    on = false;
+                    animator.SetBool("Activate", on);
+                    pipe.GetComponent<GasPipeGreen>().animator.SetBool("Active", on);
+                    pipe.transform.GetChild(0).GetComponent<BoxCollider2D>().enabled = on;
+                }
+            }
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Shady"))
+        {
+            isInRange = true;
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Shady"))
+        {
+            isInRange = false;
+        }
+
+    }
+}
---
+++ Assets/Scripts/TriggerPipe.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerPipe : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float moveSpeed = 15f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Sparky")
+        {
+            SoundMangerScript.PlaySound("gasPipe");
+            collision.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector3(0, moveSpeed, 0);
+        }
+    }
+}
